7e60ce3d252ed233381a27af28d5a39617e70637
David Banks <amoebae@gmail.com>
2019-02-18 15:10:52 +0000

    tuple functions

--- Chapter4.hs
+++ Chapter4.hs
@@ -18,3 +18,8 @@ someValue = toInteger (minBound :: Int8)
 
 davesTwo :: Integer
 davesTwo = 2
+
+-- It's a two argument function that is going to combine its tuple arguments
+-- in a certain way.
+tupFunc :: (Int, [a]) -> (Int, [a]) -> (Int, [a])
+tupFunc (a, b) (c, d) = ((a + c), (b ++ d))
