6968b6ac342fdc09f1120df8bdacbfd920bf1b87
David Banks <amoebae@gmail.com>
2019-05-15 17:28:53 +0100

    adding eq instance

--- Chapter6.hs
+++ Chapter6.hs
@@ -6,3 +6,17 @@ data Trivial = Trivial'
 
 instance Eq Trivial where
   Trivial' == Trivial' = True
+
+data DayOfWeek = Mon | Tue | Wed | Thu | Fri | Sat | Sun
+
+instance Eq DayOfWeek where
+  (==) Mon Mon = True
+  (==) Tue Tue = True
+  (==) Wed Wed = True
+  (==) Thu Thu = True
+  (==) Fri Fri = True
+  (==) Sat Sat = True
+  (==) Sun Sun = True
+  (==) _   _   = False
+
+data Date = Date DayOfWeek Int
