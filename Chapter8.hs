7ba0d25e3f7d969e1e61c2f74a5850028c1aca3d
David Banks <amoebae@gmail.com>
2019-07-14 14:27:06 +0100

    use of maybe and bottom

--- Chapter8.hs
+++ Chapter8.hs
@@ -47,3 +47,17 @@ applyTimesTail n f seed = applyTimesTail (n - 1) f (f seed)
 applyTimesComp :: (Eq a, Num a) => a -> (b -> b) -> b -> b
 applyTimesComp 0 f seed = seed
 applyTimesComp n f seed = (f . (applyTimesComp (n - 1) f)) seed
+
+-- A function that evaluates to bottom.
+
+f :: Bool -> Int
+f True = error "blah"
+f False = 0
+
+
+-- The real maybe derives show, and also seems to derive eq.
+data Maybe' a = Nothing' | Just' a
+
+f2 :: Bool -> Maybe Int
+f2 False = Just 0
+f2 True = Nothing
