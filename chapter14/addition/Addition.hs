f53e15539aeb1ad00e1ac55fc0b987846a03d72d
David Banks <amoebae@gmail.com>
2020-02-12 17:22:32 +0000

    red test for division

--- chapter14/addition/Addition.hs
+++ chapter14/addition/Addition.hs
@@ -2,12 +2,16 @@ module Addition where
 
 import Test.Hspec
 
+dividedBy :: a -> a -> (a, a)
+dividedBy x y = (x,y)
 
 main :: IO ()
 main = hspec $ do
-  describe "Addition" $ do
-    it "2 + 2 is equal to 4" $ do
-      (2 + 2) == 4 `shouldBe` True
-    it "1 + 1 is greater than 1" $ do
-      (1 + 1) > 1 `shouldBe` True
+  describe "Division" $ do
+    it "15 divided by 3 is 5" $ do
+      (dividedBy 15 3) `shouldBe` (5,0)
+    it "22 divided by 5 is (4 remainder 2)" $ do
+      (dividedBy 22 5) `shouldBe` (4,2)
+
+
 
