1ca55c3b7847f5ec1584b61d6c078c2483bacdff
David Banks <amoebae@gmail.com>
2020-11-19 08:18:56 +0000

    more tests for chapter 14 exercises

--- ExercisesChapter14.hs
+++ ExercisesChapter14.hs
@@ -10,5 +10,17 @@ main = hspec $ do
       digitToWord 0 `shouldBe` "zero"
     it "return one for 1" $ do
       digitToWord 1 `shouldBe` "one"
+
+  describe "getIndividualDigits" $ do
+    it "returns [1] for 1" $ do
+      getIndividualDigits 1 `shouldBe` [1]
+    it "returns [1, 0, 0] for 100" $ do
+      getIndividualDigits 100 `shouldBe` [1, 0, 0]
+
+  describe "wordNumber" $ do
+    it "one-zero-zero given 100" $ do
+      wordNumber 100 `shouldBe` "one-zero-zero"
+    it "nine-zero-zero-one for 9001" $ do
+      wordNumber 9001 `shouldBe` "nine-zero-zero-one"
   
 ch14 = "foo"
