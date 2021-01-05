09bb84da7f742eca6a399db5932e8a148558318c
David Banks <amoebae@gmail.com>
2021-01-05 08:07:51 +0000

    explanatory comments

--- MaybeAnotherMonoid.hs
+++ MaybeAnotherMonoid.hs
@@ -1,6 +1,12 @@
 module MaybeAnotherMonoid where
 
 -- Solution to ch15 this is a bit complicated.
+-- Write a Monoid instance for a Maybe type that doesn’t require a Monoid
+-- for the contents.
+-- This is constructed in contrast to the previous exercise that used Optional,
+-- this defined the Monoid instance on the Optional type itself and combined the
+-- contents using mappend, as opposed to this newtype which hangs the specific
+-- behaviour on the auxiliary type "First'".
 
 import Data.Monoid
 import Test.QuickCheck
@@ -22,13 +28,14 @@ monoidRightIdentity x = (x <> mempty) == x
 monoidLeftIdentity :: (Eq m, Monoid m) => m -> Bool
 monoidLeftIdentity x = (mempty <> x) == x
 
-
+-- Note that we need an Arbitrary instance on the 'a' type.
 instance Arbitrary a => Arbitrary (First' a) where
   arbitrary = do
     a <- arbitrary
     frequency [(1, return (First' Nada)), (1, return (First' (Only a)))]
 
 
+-- Always take the left if possible.  
 instance Semigroup (First' a) where
   (<>) (First' (Only x)) _ = First' (Only x)
   (<>) _ (First' (Only x)) = First' (Only x)
